0f8c08cfac2a9e50ffaf86368716cf38f75a6466
juanfonsecadev <juanof2000@gmail.com>
2024-10-09 20:20:08 -0300

    Adição da nav-bar e modulo de estilização

--- hub-studio/src/app/components/Navbar/Navbar.js
+++ hub-studio/src/app/components/Navbar/Navbar.js
@@ -1,16 +1,22 @@
 import Image from "next/image"
 import styles from "./Navbar.module.css"
+import Link from "next/link"
 
 export default function Navbar(){
     return (
         <nav className={styles.navbar}>
-            <container>
-                <div>
+            <container className={styles.NavContainer}>
+                <div className={styles.Logo}>
                     {/* Logo Image Here */}
                     <Image src={""} alt="Logo-img"/>
                 </div>
                 <div>
-                    {/* Nav Links Here */}
+                    {/* Entrar - registrar-se*/}
+
+                    <ul className={styles.navLinks}>
+                        <Link href={"/"}>   Entrar </Link> 
+                        <Link href={"/"}>   Registrar-se </Link>
+                    </ul> 
                 </div>
             </container>
         </nav>
